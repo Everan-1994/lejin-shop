1ba1b31c9fdf849e905d0dc1bedd6b9743c778b2
Everan <Everan@hywl.com>
2020-06-24 17:01:56 +0800

    完成后台订单发货

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -8,6 +8,9 @@ use Dcat\Admin\Grid;
 use Dcat\Admin\Layout\Content;
 use Dcat\Admin\Show;
 use Dcat\Admin\Controllers\AdminController;
+use Illuminate\Http\Request;
+use App\Exceptions\InvalidRequestException;
+use Illuminate\Support\Facades\Validator;
 
 class OrdersController extends AdminController
 {
@@ -26,10 +29,10 @@ class OrdersController extends AdminController
             $grid->column('user.name', '买家');
             $grid->total_amount('总金额')->sortable();
             $grid->paid_at('支付时间')->sortable();
-            $grid->ship_status('物流')->display(function($value) {
+            $grid->ship_status('物流')->display(function ($value) {
                 return Order::$shipStatusMap[$value];
             });
-            $grid->refund_status('退款状态')->display(function($value) {
+            $grid->refund_status('退款状态')->display(function ($value) {
                 return Order::$refundStatusMap[$value];
             });
             // 禁用创建按钮，后台不需要创建订单
@@ -59,5 +62,47 @@ class OrdersController extends AdminController
             ->header('查看订单')
             // body 方法可以接受 Laravel 的视图作为参数
             ->body(view('admin.orders.show', ['order' => Order::find($id)]));
+    }
+
+    /**
+     * @param Order $order
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws InvalidRequestException
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function ship(Order $order, Request $request)
+    {
+        // 判断当前订单是否已支付
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未付款');
+        }
+        // 判断当前订单发货状态是否为未发货
+        if ($order->ship_status !== Order::SHIP_STATUS_PENDING) {
+            throw new InvalidRequestException('该订单已发货');
+        }
+        // Laravel 5.5 之后 validate 方法可以返回校验过的值
+        $validator = Validator::make($request->all(), [
+            'express_company' => ['required'],
+            'express_no' => ['required'],
+        ], [], [
+            'express_company' => '物流公司',
+            'express_no' => '物流单号',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator);
+        }
+
+        // 将订单发货状态改为已发货，并存入物流信息
+        $order->update([
+            'ship_status' => Order::SHIP_STATUS_DELIVERED,
+            // 我们在 Order 模型的 $casts 属性里指明了 ship_data 是一个数组
+            // 因此这里可以直接把数组传过去
+            'ship_data' => $request->only(['express_company', 'express_no']),
+        ]);
+
+        // 返回上一页
+        return redirect()->back();
     }
 }
